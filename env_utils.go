08a80dc3f761f34fa4061a096bbff0a73f0b670c
Brance Boren <branceboren@gmail.com>
2018-06-15 03:18:47 -0600

    Added ReadEnv and WriteEnv functions. Uses aes encryption in between reading and writing

--- env_utils.go
+++ env_utils.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"encoding/json"
   "io/ioutil"
 	"fmt"
-	//"encoding/json"
+
+	"environmate/libs/encryption"
 )
 
-var env string
 type Var struct {
 	Name	string `json:"name"`
 	Value string `json:"value"`
@@ -15,16 +16,37 @@ type Environment struct {
 	Vars [] Var `json:"vars"`
 }
 
-func loadEnv(env string) {
-  data, err := ioutil.ReadFile(fmt.Sprintf("%s.json.encrypted", env))
-  if err != nil {
-    fmt.Println("Error")
-    fmt.Println(err)
-  } else {
-    fmt.Println(string(data))
-  }
+func ReadEnv(env string) Environment {
+	var e Environment
+	key := []byte("ehIEOgie$4c~rVy{[;U_;&.&-K9gV&yp")
+  data, _ := ioutil.ReadFile(fmt.Sprintf("%s.encrypted", env))
+	envJson, _ := aes.Decrypt(key, string(data))
+	err := json.Unmarshal([]byte(envJson), &e)
+	if err != nil {
+		panic(err)
+	}
+	return e
+}
+
+func WriteEnv(env string, envData Environment) {
+	key := []byte("ehIEOgie$4c~rVy{[;U_;&.&-K9gV&yp")
+	envJson, _ := json.Marshal(envData)
+	encryptedJson, _ := aes.Encrypt(key, string(envJson))
+	if err := ioutil.WriteFile(fmt.Sprintf("%v.encrypted", env), []byte(encryptedJson), 0644); err != nil {
+		fmt.Println(fmt.Sprintf("There was a problem writing the env (%v)", env))
+	}
 }
 
 func main() {
-  loadEnv("testing")
+	createEnv := Environment{}
+	createEnv.Vars = make([]Var, 0)
+	v := Var{
+		Name: "blah",
+		Value: "blahblah",
+	}
+	createEnv.Vars = append(createEnv.Vars, v)
+	WriteEnv("local", createEnv)
+	env := ReadEnv("local")
+	fmt.Println(env)
+	fmt.Println(env.Vars[0].Name)
 }
